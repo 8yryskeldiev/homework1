5d2fd8e75419d60854b9c67a104c6b01c3735cf8
8yryskeldiev <syimyk.yryskeldiev.01@gmail.com>
2023-08-05 06:04:14 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String myFirstString;
+        final int num = 1;
+        String word = " день";
+        myFirstString = num + word;
+        System.out.println(myFirstString + " " + word + " " + num);
+
+        if (num < 0) {
+            System.out.println("Вы сохранили отрицательное число");
+        } else if (num > 0) {
+            System.out.println("Вы сохранили положительное число");
+        } else {
+            System.out.println("Вы сохранили ноль");
+        }
+
+        System.out.print("Введите ваше имя: ");
+        Scanner scanner = new Scanner(System.in);
+       String name= scanner.nextLine();
+        System.out.println("Здравствуйте "+ name);
+    }
+}
